b5126d67cd41b7c2d5b4d1a8909fc115bf1fcfb2
Jack Oliver <jackoli3@me.com>
2017-05-06 17:27:22 +0800

    init commit

--- phasergame/app.js
+++ phasergame/app.js
@@ -34,7 +34,7 @@ function create(){
 	player.animations.add('left', [0,1,2,3], 10, true);
 	player.animations.add('right', [5,6,7,8], 10, true);
 	// applying physics to player
-	player.physics.arcade.enable(player);
+	game.physics.arcade.enable(player);
 	player.body.bounce.y = 0.2;
 	player.body.gravity.y = 320;
 	player.body.collideWorldGrounds = true;
